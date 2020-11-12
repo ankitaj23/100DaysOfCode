5b3bf070a98531afd8184da5a599c33d1574e3a2
ankita <ankitajha23497@gmail.com>
2020-11-12 15:55:32 +0530

    Initial Commit : Day 1 of #100DaysOfCode Solved 4 basic problems using While Loops.

---
+++ src/whileloop/try1.java
@@ -0,0 +1,90 @@
+package whileloop;
+
+import java.util.Scanner;
+
+//example 1-------print 1 to 10 numbers
+
+
+
+//public class try1 {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int i = 1;
+//        while(i<=n){
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//    }
+//}
+
+
+//example 2-----------calculate the sum of first 10 natural numbers
+
+//public  class try1{
+//    public static void main(String[] args) {
+//     Scanner   sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int i=1;
+//        int sum=0;
+//        while(i<=n){
+//            sum+=i;
+//            i++;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+//example 3 times table;
+
+//public  class try1{
+//    public static void main(String[] args) {
+//        Scanner   sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int i=1;
+//        int mul=1;
+//        while(i<=10){
+//            mul=n*i;
+//            System.out.println(mul);
+//            i++;
+//        }
+//        //System.out.println(mul);
+//    }
+//}
+
+
+//ex 4------factorial value of any number
+
+//public  class try1{
+//    public static void main(String[] args) {
+//        Scanner   sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int i=1;
+//        int sum=0;
+//        while(i<=n){
+//            sum+=i;
+//            i++;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+
+//--example Two numbers are entered through the keyboard.
+// Write a program to find the value
+// of one number raised to the power of another. (Do not use Java built-in method)
+public  class try1{
+    public static void main(String[] args) {
+        Scanner   sc=new Scanner(System.in);
+        int base=sc.nextInt();
+        int exponent=sc.nextInt();
+        int i=1;
+        int sum=0;
+        while(i<=n){
+            sum+=i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
